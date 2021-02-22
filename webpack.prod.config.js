809a487e6e703d808f8c167e670010daa57b97cd
Igor Kim <igkim90@gmail.com>
2021-02-22 11:32:17 +0500

    fix: builds

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.js',
+  externals: [nodeExternals()],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
